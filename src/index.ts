f9889e8ae067afd8e4098de7772e23a464b8bfbe
sevro49 <21120205303@ismu.edu.tr>
2024-03-09 16:12:43 +0300

    literal types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-type Draggable = {
-	drag: () => void;
-};
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
 
-type Resizable = {
-	resize: () => void;
-};
-
-type UIWidget = Draggable & Resizable;
-
-let textBox: UIWidget = {
-    drag: () => {},
-    resize: () => {}
-}
+type Metric = "cm" | "inch";
