6a12d119b2c40cfbfb3a0404501cb2299c26da2b
sevro49 <21120205303@ismu.edu.tr>
2024-03-09 15:59:05 +0300

    objects

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-function calculateTax(income: number, taxYear = 2022): number {
-	if (taxYear < 2022) return income * 1.2;
-	return income * 1.3;
-}
-
-calculateTax(10_000, 2022);
+let employee: {
+	readonly id: number;
+	name: string;
+	retire: (date: Date) => void;
+} = {
+	id: 1,
+	name: "Emre",
+	retire: (date: Date) => {
+		console.log(date);
+	},
+};
+// employee.id = 10;
