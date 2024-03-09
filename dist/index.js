18f22b1779291618be5967f0af888f6bc0dcaec1
sevro49 <21120205303@ismu.edu.tr>
2024-03-09 12:12:43 +0300

    the any type and built-in-types

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+console.log(typeof sales);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-let age: number = 20;
-if (age < 50) 
-    age += 10
-console.log(age)
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+let level; //type any
+
+function render (document: any ) {
+    console.log(document)
+}
