5338cc7f8ecccbcdae23205f10b648d5f6cc7305
sevro49 <21120205303@ismu.edu.tr>
2024-03-09 12:39:36 +0300

    functions

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,8 @@
 "use strict";
-let mySize = 2;
-console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000, 2022);
 //# sourceMappingURL=index.js.map
